5712c5a0c36f617bc6db956b8cdf33b732020f50
Joe Leveille <joeleve212@gmail.com>
2025-01-06 20:03:09 -0700

    Separate modules into separate files

--- initialTests/cu_top.v
+++ initialTests/cu_top.v
@@ -25,26 +25,3 @@ module cu_top(
     
 endmodule
 
-
-module bcd_oneChar(
-  input [3:0] binDig,
-  output [3:0] segOut
-  );
-  //binDig[3] is A
-  //binDig[2] is B
-  //binDig[1] is C
-  //binDig[0] is D
-  
-  //seg a
-  assign segOut[0] = ~(binDig[0] ^ binDig[2]) | binDig[3] | binDig[1];
-  //seg b
-  assign segOut[1] = ~(binDig[2]) | ~(binDig[1] ^ binDig[0]);
-  //seg c
-  assign segOut[2] = ~(binDig[1]) | binDig[0] | binDig[2];
-  //seg d
-  assign segOut[3] = (~binDig[2] & ~binDig[0]) | (binDig[1] & ~binDig[0]) | 
-              (binDig[1] & ~binDig[2]) | (binDig[2] & ~binDig[1] & binDig[0]) | binDig[3];
-
-  
-  
-endmodule

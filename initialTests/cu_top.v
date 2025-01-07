0284d8fdee13f78acea51fd2fbd40f3931d0b0f8
Joe Leveille <joeleve212@gmail.com>
2025-01-06 19:02:17 -0700

    Setup gitignore for workaround, add a-d to bcd

--- initialTests/cu_top.v
+++ initialTests/cu_top.v
@@ -15,25 +15,41 @@ module cu_top(
     output [7:0] io_seg
     );
     
+    //DEFAULT ITEMS::
     wire rst;
-    
     // The reset conditioner is used to synchronize the reset signal to the FPGA
     // clock. This ensures the entire FPGA comes out of reset at the same time.
     reset_conditioner reset_conditioner(.clk(clk), .in(!rst_n), .out(rst));
-    
-    assign led = 8'h00;      // turn LEDs off
 
     assign usb_tx = usb_rx;  // echo the serial data
+
+    //MY DESIGN::
+    reg [3:0] binaryVal;
+    bcd_oneChar(io_dip0[3:0], io_seg[3:0]);
+
     
 endmodule
 
 
 module bcd_oneChar(
   input [3:0] binDig,
-  output [7:0] segOut
+  output [3:0] segOut
   );
+  //binDig[3] is A
+  //binDig[2] is B
+  //binDig[1] is C
+  //binDig[0] is D
   
-  assign segOut = binDig;
+  //seg a
+  assign segOut[0] = ~(binDig[0] ^ binDig[2]) | binDig[3] | binDig[1];
+  //seg b
+  assign segOut[1] = ~(binDig[2]) | ~(binDig[1] ^ binDig[0]);
+  //seg c
+  assign segOut[2] = ~(binDig[1]) | binDig[0] | binDig[2];
+  //seg d
+  assign segOut[3] = (~binDig[2] & ~binDig[0]) | (binDig[1] & ~binDig[0]) | 
+              (binDig[1] & ~binDig[2]) | (binDig[2] & ~binDig[1] & binDig[0]) | binDig[3];
+
   
   
 endmodule
